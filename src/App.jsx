39d8af79dab6973e544a40743ff2dd5397afc39f
AZARKANE Issam <issam.azarkane@gmail.com>
2023-12-25 16:40:42 +0100

    Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar/Navbar"
+import Banner from "./components/Banner/Banner"
 
 function App() {
 
   return (
     <div className="container mx-auto">
       <Navbar />
+      <Banner />
     </div>
   )
 }
