5852c53e971c46bbbaef2d6e2554ed5ea2e10262
AZARKANE Issam <issam.azarkane@gmail.com>
2023-12-27 16:00:23 +0100

    contact form + formspree

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Banner from "./components/Banner/Banner"
 import Services from "./components/Services/Services"
 import About from "./components/About/About"
 import Team from "./components/Team/Team"
+import Contact from "./components/Contact/Contact"
 
 function App() {
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto relative">
       <Navbar />
       <Banner />
       <Services />
       <About />
       <Team />
+      <Contact />
     </div>
   )
 }
